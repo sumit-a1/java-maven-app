f8bebe4506e59bb5cfb797f8c60fafa674f8b797
Sumit Jha <sumitjha1@outlook.com>
2023-07-04 03:55:38 +0530

    correct script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
 	echo "building the application..."
-	withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', UsernameVariable: 'USER')]) {
+	withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
 		sh 'docker build -t sumitjha1/my-repo:jma-2.1 .'
 		sh "echo $PASS | docker login -u $USER --password-stdin"
 		sh 'docker push sumitjha1/my-repo:jma-2.1'
